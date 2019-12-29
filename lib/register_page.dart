d023eebe4a977bd3fa394944ec78bd3608b4f5b9
Arshdeep Ganda <arshdeep.ganda@alumni.ubc.ca>
2019-12-28 22:16:07 -0800

    More regex, post a created user to backend

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -8,32 +9,37 @@ class RegisterPage extends StatefulWidget {
 
 class _RegisterPageState extends State<RegisterPage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _nameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _firstNameController = TextEditingController();
-  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   String emailValidator(String value) {
-    if (value.isEmpty) {
-      return 'Please enter a valid e-mail address';
+    RegExp exp = RegExp(
+        r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
+    if (exp.hasMatch(value)) {
+      return null;
     }
-    return null;
+    return 'Please enter a valid e-mail address';
   }
 
   String passwordValidator(String value) {
-    if (value.isEmpty) {
-      return 'Please enter a valid password';
+    RegExp exp = RegExp(r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$");
+    if (exp.hasMatch(value)) {
+      return null;
     }
-    return null;
+    return 'Please enter a password that has at least one letter, one number, and 8 characters long.';
   }
 
   String nameValidator(String value) {
-    if (value.isEmpty) {
-      return 'Please enter a valid name';
+    RegExp exp = RegExp(r"^([a-zA-ZÀ-ÿ\u00f1\u00d1]+[ \-]{0,1})+$");
+    if (exp.hasMatch(value)) {
+      return null;
     }
-    return null;
+    return 'Please enter a valid name.';
   }
 
   @override
@@ -44,6 +50,13 @@ class _RegisterPageState extends State<RegisterPage> {
         key: _formKey,
         child: Column(
           children: <Widget>[
+            TextFormField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                hintText: 'Full name',
+              ),
+              validator: nameValidator,
+            ),
             TextFormField(
               controller: _emailController,
               decoration: InputDecoration(
@@ -59,29 +72,44 @@ class _RegisterPageState extends State<RegisterPage> {
               validator: passwordValidator,
             ),
             TextFormField(
-              controller: _firstNameController,
+              controller: _confirmPasswordController,
               decoration: InputDecoration(
-                hintText: 'First name',
+                hintText: 'Re-enter password',
               ),
-              validator: nameValidator,
-            ),
-            TextFormField(
-              controller: _lastNameController,
-              decoration: InputDecoration(
-                hintText: 'Last name',
-              ),
-              validator: nameValidator,
+              validator: (value) {
+                if (value != _passwordController.text) {
+                  return 'Passwords do not match.';
+                }
+                return null;
+              },
             ),
             RaisedButton(
               child: Text('Submit'),
               onPressed: () async {
                 if (_formKey.currentState.validate()) {
-                  FirebaseUser _user =
+                  FirebaseUser user =
                       (await _auth.createUserWithEmailAndPassword(
                               email: _emailController.text,
                               password: _passwordController.text))
                           .user;
-                  return true;
+                  if (user != null) {
+                    //do something
+                    UserUpdateInfo uuInfo = UserUpdateInfo();
+                    uuInfo.displayName = _nameController.text;
+                    uuInfo.photoUrl = "";
+                    await user.updateProfile(uuInfo);
+                    await user.sendEmailVerification();
+
+                    Dio dio = new Dio();
+                    dio.options.baseUrl = "https://abode-backend.herokuapp.com";
+                    await dio.post("/user", data: {
+                      "firebaseId": user.uid,
+                      "name": _nameController.text,
+                      "email": _emailController.text
+                    });
+                  } else {
+                    // do something else
+                  }
                 }
                 return null;
               },

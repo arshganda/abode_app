abd25f38efef8d0f88efbae522c151042c3fe3c2
Arshdeep Ganda <arshdeep.ganda@alumni.ubc.ca>
2019-12-29 17:55:58 -0800

    Text field rounded w/ drop shadow

--- lib/forgot_password_page.dart
+++ lib/forgot_password_page.dart
@@ -10,44 +10,67 @@ class ForgotPasswordPage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Forgot Password?'),
+        title: Text('Forgot Password'),
       ),
       body: SafeArea(
         child: Container(
+            padding: EdgeInsets.fromLTRB(16, 16, 16, 0),
+            color: Theme.of(context).backgroundColor,
             child: Column(
-          children: <Widget>[
-            Text("Please enter your registered e-mail"),
-            TextFormField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                border: OutlineInputBorder(),
-                hintText: 'E-mail',
-              ),
-              validator: emailValidator,
-              keyboardType: TextInputType.emailAddress,
-            ),
-            RaisedButton(
-              child: Text('Send'),
-            ),
-            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text("Don't have an account?"),
-                FlatButton(
-                  child: Text('Sign up'),
+                Spacer(),
+                Text("Please enter your registered e-mail"),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(32)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 6.0,
+                        offset: Offset(0, 2.0),
+                      ),
+                    ],
+                  ),
+                  child: TextFormField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'E-mail',
+                      contentPadding: EdgeInsets.all(16),
+                    ),
+                    validator: emailValidator,
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                ),
+                RaisedButton(
+                  child: Text('Send'),
+                  color: Theme.of(context).accentColor,
+                  textColor: Colors.white,
                   onPressed: () {
-                    Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                            builder: (BuildContext context) => RegisterPage()));
+                    return 5;
                   },
                 ),
+                Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Don't have an account?"),
+                    FlatButton(
+                      child: Text('Sign up'),
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    RegisterPage()));
+                      },
+                    ),
+                  ],
+                ),
               ],
-            ),
-          ],
-        )),
+            )),
       ),
     );
   }
